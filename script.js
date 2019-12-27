1ddf56c5c7fbf6e3402c9f6327add91a15fa02ad
Nicholas Nascimento <nicholas.an99@gmail.com>
2019-12-27 15:35:03 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function locdoc() {
+    window.location.href = "https://github.com/gitobioinformatics/gito";
+}
+
+function gogit() {
+    window.location.href = "https://github.com/gitobioinformatics/gito";
+}
